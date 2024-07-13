6101da5d73c0d822319c71eb971ce1f2a94ffcb0
technicalnbfc <technical@northbrisbanefc.com.au>
2024-07-13 12:51:42 +1000

    bump bootstrap version 5.3.2 to 5.3.3

--- js/color_mode.js
+++ js/color_mode.js
@@ -23,10 +23,8 @@
 
   const setTheme = (theme) => {
     if (
-      theme === "auto" &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
-      document.documentElement.setAttribute("data-bs-theme", "dark");
+      theme === "auto") {
+      document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
     } else {
       document.documentElement.setAttribute("data-bs-theme", theme);
     }
